d8c8e1f3ddc22bcfc6e3b9ab39933a36932fde75
chandu <cmhandan@gmail.com>
2024-05-24 23:42:44 +0530

    added layout for the home page

---
+++ frontend/src/Layout/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet  } from 'react-router-dom'
+import Header from '../pages/Header'
+import Footer from '../pages/Footer'
+import Home from '../pages/Home'
+export default function HomeLayout() {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <Footer/>
+    </>
+  )
+}
