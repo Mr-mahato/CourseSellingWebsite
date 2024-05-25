a2db7292aac0dcc6141c75a4e65d7bca6494a171
chandu <cmhandan@gmail.com>
2024-05-25 21:54:10 +0530

    added signin route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./pages/Header";
 import HomeLayout from "./Layout/HomeLayout";
 import Signup from "./pages/Signup";
+import SignIn from "./pages/SignIn";
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomeLayout/>}/>
         <Route path="/signup" element={<Signup/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
       </Routes>
     </div>
   );
