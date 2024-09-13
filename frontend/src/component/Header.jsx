a2965905b704d5a1cf983d343ea64b42a1f1da0c
Mr-mahato <cmhandan@gmail.com>
2024-09-13 23:42:02 +0530

    Refactor Header component and improve responsiveness

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -4,18 +4,15 @@ import "../styles/Header.css";
   @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 </style>;
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBookOpen, faBars,faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faBookOpen, faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-import { Avatar, Menu, MenuItem } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { AuthContext } from "../context/authContext";
-import Login from "./Login";
-import Signup from "./Signup";
+import UserLoginModel from "./Login/UserLoginModel";
 
 function Header() {
-  const [isLogin, setIsLogin] = useState(false);
-  const [isSignup, setIsSignup] = useState(false);
+  const [userLoginModel, setUserLoginModel] = useState(false);
   const [sideBarModel, setSideBarModel] = useState(false);
   const [scrolly, setScrolly] = useState(0);
 
@@ -45,116 +42,101 @@ function Header() {
   };
 
   return (
-    <nav
-      className={`fixed w-full top-0 z-20 ${
-        scrolly < 500 && "border-b-2 border-blue-600"
-      }`}
-    >
-      <div id="title">
-        <FontAwesomeIcon icon={faBookOpen} style={{ height: "1.3em" }} />
-        <h1>
-          EDU<span id="care">Care</span>
-        </h1>
-      </div>
+    <div>
+      <nav
+        className={`fixed w-full top-0 z-20 ${
+          scrolly < 500 && "border-b-2 border-blue-600"
+        }`}
+      >
+        <div id="title">
+          <FontAwesomeIcon icon={faBookOpen} style={{ height: "1.3em" }} />
+          <h1>
+            EDU<span id="care">Care</span>
+          </h1>
+        </div>
 
-      {/* different navigation here */}
-      <div className="md:block hidden">
-        <ul className="mid-list">
-          <Link to={"/"} className="focus:text-blue-600">
-            Home
-          </Link>
-          <Link to={"/courses"} className="focus:text-blue-600">
-            All Courses
-          </Link>
-          <Link to={"/contact"} className="focus:text-blue-600">
-            Contact
-          </Link>
-        </ul>
-      </div>
+        {/* different navigation here */}
+        <div className="md:block hidden">
+          <ul className="mid-list">
+            <Link to={"/"} className="focus:text-blue-600">
+              Home
+            </Link>
+            <Link to={"/courses"} className="focus:text-blue-600">
+              All Courses
+            </Link>
+            <Link to={"/contact"} className="focus:text-blue-600">
+              Contact
+            </Link>
+          </ul>
+        </div>
 
-      {/* hamburger section */}
-      <div className="md:hidden block">
-        <FontAwesomeIcon
-          onClick={() => setSideBarModel(!sideBarModel)}
-          className="cursor-pointer"
-          icon={faBars}
-          style={{ height: "1.3em" }}
-        />
+        {/* hamburger section */}
+        <div className="md:hidden block">
+          <FontAwesomeIcon
+            onClick={() => setSideBarModel(!sideBarModel)}
+            className="cursor-pointer"
+            icon={faBars}
+            style={{ height: "1.3em" }}
+          />
 
-        {sideBarModel && (
-          <aside className="absolute  right-0 top-0 flex flex-col bg-neutral-200 min-h-screen w-[13rem]">
-            <FontAwesomeIcon onClick={() => setSideBarModel(!sideBarModel)} className="cursor-pointer self-end  " icon={faXmark} style={{height:"2rem"}} />
-            <div className="w-[80%] flex flex-col justify-between min-h-[90vh] mx-auto pt-10">
-              <div className="flex flex-col">
-                <Link to={"/"} className="hover:bg-neutral-300 px-2 py-1">
-                  Home
-                </Link>
-                <Link
-                  to={"/courses"}
-                  className="hover:bg-neutral-300 px-2 py-1"
-                >
-                  All Courses
-                </Link>
-                <Link
-                  to={"/contact"}
-                  className="hover:bg-neutral-300 px-2 py-1"
-                >
-                  Contact
-                </Link>
+          {sideBarModel && (
+            <aside className="absolute  right-0 top-0 flex flex-col bg-neutral-200 min-h-screen w-[13rem]">
+              <FontAwesomeIcon
+                onClick={() => setSideBarModel(!sideBarModel)}
+                className="cursor-pointer self-end  "
+                icon={faXmark}
+                style={{ height: "2rem" }}
+              />
+              <div className="w-[80%] flex flex-col justify-between min-h-[90vh] mx-auto pt-10">
+                <div className="flex flex-col">
+                  <Link to={"/"} className="hover:bg-neutral-300 px-2 py-1">
+                    Home
+                  </Link>
+                  <Link
+                    to={"/courses"}
+                    className="hover:bg-neutral-300 px-2 py-1"
+                  >
+                    All Courses
+                  </Link>
+                  <Link
+                    to={"/contact"}
+                    className="hover:bg-neutral-300 px-2 py-1"
+                  >
+                    Contact
+                  </Link>
+                </div>
+
+                <div className=" flex justify-between">
+                  <button
+                    onClick={() => setIsLogin(true)}
+                    className=" p-2 font-semibold text-neutral-600"
+                  >
+                    Log In
+                  </button>
+                  <button
+                    onClick={() => setIsSignup(true)}
+                    className="bg-blue-500 px-2  ml-2 ring-2 ring-blue-800 text-white font-semibold hover:bg-blue-600/90"
+                  >
+                    {" "}
+                    Sign Up
+                  </button>
+                </div>
               </div>
-
-              <div className=" flex justify-between">
-                <button
-                  onClick={() => setIsLogin(true)}
-                  className=" p-2 font-semibold text-neutral-600"
-                >
-                  Log In
-                </button>
-                <button
-                  onClick={() => setIsSignup(true)}
-                  className="bg-blue-500 px-2  ml-2 ring-2 ring-blue-800 text-white font-semibold hover:bg-blue-600/90"
-                >
-                  {" "}
-                  Sign Up
-                </button>
-              </div>
-            </div>
-          </aside>
-        )}
-      </div>
-
-      {/* login signup */}
- 
-        <div className="md:flex hidden gap-2">
+            </aside>
+          )}
+        </div>
+        {/* #TODO:add login and signup logic here  */}
+        <div>
           <button
-            onClick={() => setIsLogin(true)}
-            className="border rounded-sm border-blue-600 font-semibold text-neutral-600 px-4 py-2"
+            onClick={() => setUserLoginModel(!userLoginModel)}
+            className="bg-sky-300 px-3 py-2 rounded-md text-white font-semibold"
           >
-            Log In
-          </button>
-          <button
-            onClick={() => setIsSignup(true)}
-            className="bg-blue-500 rounded-sm  px-4 py-2 ml-2 border border-blue-800 text-white font-semibold hover:bg-blue-600/90"
-          >
-            {" "}
-            Sign Up
+            Login
           </button>
         </div>
+        {/* login signup */}
 
-      {isLogin ? (
-        <div className="fixed inset-0 flex z-20 items-center">
-          <Login setIsLogin={setIsLogin} />
-        </div>
-      ) : null}
-
-      {isSignup ? (
-        <div className="fixed inset-0 flex z-20 items-center">
-          <Signup setIsSignup={setIsSignup} />
-        </div>
-      ) : null}
-
-      {/* user profile showing */}
-      {/* {isAuthenticated && (
+        {/* {isAuthenticated && (
         <div className="userLoggedHandel">
           <Avatar
             sx={{ cursor: "pointer" }}
@@ -178,7 +160,9 @@ function Header() {
           </Menu>
         </div>
       )} */}
-    </nav>
+      </nav>
+      {userLoginModel && <UserLoginModel />}
+    </div>
   );
 }
 
